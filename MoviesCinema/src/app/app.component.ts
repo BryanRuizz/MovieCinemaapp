c868211a3a3e5e72490d5ac78fc1c0931e3167d2
bryan ruiz <andyruiz2468@gmail.com>
2024-08-15 18:48:46 -0600

    feat: working on architecture
    need an fild api

--- MoviesCinema/src/app/app.component.ts
+++ MoviesCinema/src/app/app.component.ts
@@ -12,12 +12,17 @@ export class AppComponent {
     { id: 2, name: 'picaso 2', favorite: false, imageUrl: 'path/to/image2.jpg' },
     { id: 3, name: 'yuya 1', favorite: true, imageUrl: 'path/to/image1.jpg' },
     { id: 4, name: 'yei 2', favorite: false, imageUrl: 'path/to/image2.jpg' },
+    
     // Añade más ítems según sea necesario
   ];
 
-  addNew() {
-    // Lógica para agregar un nuevo ítem
-    console.log('Add new item');
+  constructor() { }
+
+  onAddClick(): void {
+    console.log('Card clicked!');
+  }
+  card(id:number){
+    console.log("card number", id);
   }
 
 }
